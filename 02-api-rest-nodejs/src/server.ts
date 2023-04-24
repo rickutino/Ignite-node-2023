8a6fce28a81fab8a94783b31493b17d4743856aa
rickutino <rickutino@gmail.com>
2023-04-24 17:38:51 +0900

    chore: connetion and config knex

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const server = fastify()
 
-server.get('/hello', () => {
-  return 'Hello world'
+server.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 server.listen({ port: 3333 }).then(() => {
