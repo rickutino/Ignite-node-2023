34af1bbf0211d0569362724bb609a0f64a3d050c
rickutino <rickutino@gmail.com>
2023-07-24 12:27:11 +0900

    create transactio table

---
+++ 02-api-rest-nodejs/db/migrations/20230724032432_add-session-id-to-transactions.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.uuid('session_id').after('id').index() // index() ira criar um tipo de cash na memoria melhorando o where() na hora da busca.
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.dropColumn('session_id')
+  })
+}
