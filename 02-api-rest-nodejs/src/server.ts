8527ff2dfc076d018d9d21b933e3b0f9b79a45a3
rickutino <rickutino@gmail.com>
2023-07-24 18:00:36 +0900

    Plugins of Fastify

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,14 +1,10 @@
 import fastify from 'fastify'
-import { knex } from './database'
+import { transactionsRoutes } from './routes/transactions'
 import { env } from './env'
 
 const server = fastify()
 
-server.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
-})
+server.register(transactionsRoutes)
 
 server.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running!')
