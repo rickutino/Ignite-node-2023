953694ed462b398679d33ac4f7f166d6220cf003
rickutino <rickutino@gmail.com>
2023-07-25 12:36:36 +0900

    feature: list all or list single transaction

--- 02-api-rest-nodejs/README.md
+++ 02-api-rest-nodejs/README.md
@@ -1,11 +1,11 @@
 # RF
 
 - [X] The user must be able to obtain a summary of his/her account;
-- [ ] The user must be able to list all transactions that have already occurred;
-- [ ] The user must be able to view a single transaction;
+- [X] The user must be able to list all transactions that have already occurred;
+- [X] The user must be able to view a single transaction;
 
 # RN
 
-- [ ] The transaction can be of the credit type that will add to the total amount, or debit will subtract;
+- [X] The transaction can be of the credit type that will add to the total amount, or debit will subtract;
 - [ ] It must be possible to identify the user between requests;
 - [ ] The user can only visualize transactions which he created;
--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -4,10 +4,24 @@ import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(server: FastifyInstance) {
-  server.get('/hello', async () => {
-    const transactions = await knex('transactions').select('*')
+  server.get('/', async () => {
+    const transactions = await knex('transactions').select()
 
-    return transactions
+    return { transactions }
+  })
+
+  server.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
   })
 
   server.post('/', async (request, reply) => {
